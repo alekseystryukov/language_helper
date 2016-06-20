d1494d4a3c77612e92210a76b85d8fb885259c55
upik <dopler>
2016-06-20 14:35:28 +0300

    Adding words

--- client/settings.py
+++ client/settings.py
@@ -1,4 +1,4 @@
-HOST = 'http://localhost:8000/'
+HOST = 'http://178.151.170.22:8080/'
 API_URL = HOST + 'api/v1/'
 GET_QUESTION_URL = API_URL + 'get_random_word/'
 POST_WORD_URL = API_URL + 'post_word/'
